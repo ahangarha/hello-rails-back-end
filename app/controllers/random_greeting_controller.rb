5a5df5566acabee1a492fc3aec4ab427f4345bb6
Mostafa Ahangarha <ahangarha@riseup.net>
2022-07-05 12:30:43 +0300

    add api endpoint for random greeting

---
+++ app/controllers/random_greeting_controller.rb
@@ -0,0 +1,12 @@
+class RandomGreetingController < ApplicationController
+  def index
+    greeting = Greeting.all.sample
+
+    if greeting.blank?
+      render json: {data: 'Nothing found!', status: 404}
+    else
+      greeting_text = greeting.text
+      render json: {data: greeting_text, status: 200}
+    end
+  end
+end
